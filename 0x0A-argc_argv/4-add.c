df58b7c725d363d8bdd3bf7881614f45db657fd9
debuka <bukasodean@gmail.com>
2023-06-12 17:19:42 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,34 +8,74 @@
  * @argv: array of arguements
  * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	int sum = 0;
-	int i;
-
-	if (argc > 1)
+int check_num(char *str) /*check_num - checks for positive numbers*/
+			   /*str: pointer to the array of string*/
 	{
-		for (i = 0; i > argc; i++)
+		unsigned int num;
+
+
+		num = 0;
+
+		while (num < strlen(str)) /*numbring the string*/
+
+
 		{
-			int j;
-			char *str;
-
-			str = argv[i];
-
-			for (j = 0; str[j] != '\0'; j++)
+			if (!isdigit(str[num])) /*check if str there are digit*/
 			{
-				for (str[j] >= 0.0)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				return (0);
 			}
+
+
+			num++;
 		}
+		return (1);
 	}
-for (i = 0; i < argc; i++)
-{
-	sum += atoi(argv[i]);
-}
-printf("%d\n", sum);
-return (0);
-}
+
+/**
+	 * main - Print the name of the program
+	 * @argc: Count arguments
+	 * @argv: Arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
+
+
+	int main(int argc, char *argv[])
+
+
+	{
+		/*Declaring variables*/
+		int num;
+		int str_to_int;
+		int resut = 0;
+
+
+		num = 1;
+		while (num < argc) /*Goes through the whole array*/
+		{
+			if (check_num(argv[num]))
+
+
+			{
+				str_to_int = atoi(argv[num]); /*atoi --> converts string to int*/
+				result += str_to_int;/* stores the converted string*/
+			}
+
+
+			/*If one of there's symbols not digits*/
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+
+			num++;
+		}
+
+
+		printf("%d\n", result); /*prints result*/
+
+
+		return (0);
+	}
