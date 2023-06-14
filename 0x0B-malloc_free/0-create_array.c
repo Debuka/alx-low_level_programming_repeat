c9eca2e5d283ee4af783d9257d0e755876a6e203
debuka <bukasodean@gmail.com>
2023-06-14 14:35:00 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,18 @@ char *create_array(unsigned int size, char c)
 	char *arsr;
 
 	unsigned int j;
-	
+
+	if (size == 0)
+		return (0);
+
 	arsr = malloc(sizeof(char) * size);
 
-	if (size == 0 || arsr == NULL)
+	if (arsr == NULL)
 
 		return (NULL);
-	
-	for ( = 0; j < size; j++)
+	for (j = 0; j < size; j++)
 
-		c = arsr[i];
+		arsr[j] = c;
 
 	return (arsr);
 
